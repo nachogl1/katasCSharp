df1dfe7557f65a86bdd944bbf3a9424c8b1d287c
nachogl1 <i.gonzalezl@hotmail.com>
2025-03-08 19:35:15 +0000

    First commit

---
+++ FizzBuzz/FizzBuzzParser.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace FizzBuzzParser.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [TestCase(3, "Fizz")]
+        [TestCase(6, "Fizz")]
+        [TestCase(9, "Fizz")]
+        [TestCase(12, "Fizz")]
+        public void ReturnFizz_OnlyDivisibleBy3(int input, string expected)
+        {
+            FizzBuzzParser parser = new FizzBuzzParser();
+            String result = parser.Parse(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase(5, "Buzz")]
+        [TestCase(10, "Buzz")]
+        [TestCase(20, "Buzz")]
+        [TestCase(25, "Buzz")]
+        public void ReturnBuzz_OnlyDivisibleBy5(int input, string expected)
+        {
+            FizzBuzzParser parser = new FizzBuzzParser();
+            String result = parser.Parse(input);
+            Assert.AreEqual(expected, result);
+        }
+        
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(30, "FizzBuzz")]
+        [TestCase(45, "FizzBuzz")]
+        [TestCase(60, "FizzBuzz")]
+        public void ReturnFizzBuzz_DivisibleBy5And3(int input, string expected)
+        {
+            FizzBuzzParser parser = new FizzBuzzParser();
+            String result = parser.Parse(input);
+            Assert.AreEqual(expected, result);
+        }
+        
+        
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(4, "4")]
+        [TestCase(7, "7")]
+        public void ReturnNumber_NotDivisibleBy5Nor3(int input, string expected)
+        {
+            FizzBuzzParser parser = new FizzBuzzParser();
+            String result = parser.Parse(input);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzzParser/FizzBuzzParser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FizzBuzzParser
+{
+    public class FizzBuzzParser
+    {
+        public string Parse(int input)
+        {
+            var result = "";
+
+            if (input % 3 == 0)
+            {
+                result += "Fizz";
+            }
+
+            if (input % 5 == 0)
+            {
+                result += "Buzz";
+            }
+
+
+            return result.Length > 0 ? result : Convert.ToString(input);
+        }
+    }
+}
