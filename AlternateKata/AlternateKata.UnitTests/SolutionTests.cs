a810427386a21093509e8ac186a560f9f808f87b
nachogl1 <i.gonzalezl@hotmail.com>
2025-03-14 17:25:23 +0000

    kata: alternate vowels and consonants

---
+++ AlternateKata/AlternateKata.UnitTests/SolutionTests.cs
@@ -0,0 +1,15 @@
+namespace AlternateKata.UnitTests;
+
+public class SolutionTests
+{
+
+    [TestCase("amazon",true)]
+    [TestCase("mazon",true)]
+    [TestCase("banana",true)]
+    [TestCase("aamazon",false)]
+    [TestCase("apple",false)]
+    public void ReturnTruIfAlternate(string input, bool expected)
+    {
+        Assert.That(Solution.IsAlternate(input), Is.EqualTo(expected));
+    }
+}
---
+++ AlternateKata/AlternateKata/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace AlternateKata;
+
+public class Solution
+{
+    private static HashSet<char> _vowels = ['a', 'e', 'i', 'o', 'u'];
+
+    public static bool IsAlternate(string input)
+    {
+        char firstCharacter = Char.Parse(input.Substring(0, 1));
+        return Recursive(input, 0, _vowels.Contains(firstCharacter));
+    }
+
+    private static bool Recursive(string input, int index, bool isFirstVowel)
+    {
+        if (input.Length == index) return true;
+
+        if (index % 2 == 0 && isFirstVowel && !_vowels.Contains(input[index])) return false;
+        if (index % 2 != 0 && isFirstVowel && _vowels.Contains(input[index])) return false;
+
+        return Recursive(input, ++index, isFirstVowel);
+    }
+}
